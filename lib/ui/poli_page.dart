2694175ef87cd4ff536c0921213ae011d63b79ee
l0c4lxid <syaidxandhika@gmail.com>
2023-05-11 15:26:29 +0700

    update poli_page

--- lib/ui/poli_page.dart
+++ lib/ui/poli_page.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import '../model/poli.dart';
 import 'poli_detail.dart';
+import 'poli_item.dart';
 
 class PoliPage extends StatefulWidget {
   const PoliPage({super.key});
-
   @override
   State<PoliPage> createState() => _PoliPageState();
 }
@@ -16,35 +16,10 @@ class _PoliPageState extends State<PoliPage> {
       appBar: AppBar(title: const Text("Data Poli")),
       body: ListView(
         children: [
-          GestureDetector(
-            child: Card(
-              child: ListTile(
-                title: const Text("Poli Anak"),
-              ),
-            ),
-            onTap: () {
-              Poli poliAnak = new Poli(namaPoli: "Poli Anak");
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => PoliDetail(poli: poliAnak)));
-            },
-          ),
-          Card(
-            child: ListTile(
-              title: const Text("Poli Kandungan"),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              title: const Text("Poli Gigi"),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              title: const Text("Poli THT"),
-            ),
-          ),
+          PoliItem(poli: Poli(namaPoli: "Poli Anak")),
+          PoliItem(poli: Poli(namaPoli: "Poli Kandungan")),
+          PoliItem(poli: Poli(namaPoli: "Poli Gigi")),
+          PoliItem(poli: Poli(namaPoli: "Poli THT")),
         ],
       ),
     );
