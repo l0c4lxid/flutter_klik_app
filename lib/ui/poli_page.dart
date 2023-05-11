296c9e3474c91ed30a8bccd7073e49a82715c72f
l0c4lxid <syaidxandhika@gmail.com>
2023-05-11 15:28:24 +0700

    add AppBar on poli_page

--- lib/ui/poli_page.dart
+++ lib/ui/poli_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../model/poli.dart';
 import 'poli_detail.dart';
 import 'poli_item.dart';
+import 'poli_form.dart';
 
 class PoliPage extends StatefulWidget {
   const PoliPage({super.key});
@@ -13,7 +14,18 @@ class _PoliPageState extends State<PoliPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Data Poli")),
+      appBar: AppBar(
+        title: const Text("Data Poli"),
+        actions: [
+          GestureDetector(
+            child: const Icon(Icons.add),
+            onTap: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => PoliForm()));
+            },
+          )
+        ],
+      ),
       body: ListView(
         children: [
           PoliItem(poli: Poli(namaPoli: "Poli Anak")),
